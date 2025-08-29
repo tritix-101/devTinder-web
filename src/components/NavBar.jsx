b82f896565d0292f2bf7b16141444c8bcfb9d30a
Priyanka Kandula <priyankakandula2004@gmail.com>
2025-08-29 22:35:39 +0530

    redirect to login if unauthorized

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,47 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const user=useSelector((store)=>store.user);
+  const user = useSelector((store) => store.user);
   console.log(user);
+
   return (
     <>
       <div className="navbar bg-neutral shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">👩‍💻DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">👩‍💻DevTinder</Link>
         </div>
         <div className="flex gap-2">
           {user && (
-            <div className="dropdown dropdown-end  mx-5 flex items-center">
-              <p>Welcome , {user.firstName}</p>
+            <div className="dropdown dropdown-end mx-5">
+              {/* Trigger (photo + name) */}
               <div
                 tabIndex={0}
                 role="button"
-                className="btn btn-ghost btn-circle avatar flex items-center gap-2 px-2"
+                className="btn btn-ghost flex items-center gap-2 px-2"
               >
-                <div className="w-10 rounded-full">
-                  <img alt="user photo" src={user.photoUrl} />
+                {/* Avatar container */}
+                <div className="w-10 h-10 rounded-full overflow-hidden flex items-center justify-center">
+                  <img
+                    alt="user photo"
+                    src={user.photoUrl}
+                    className="w-full h-full object-cover rounded-full object-top"
+                  />
                 </div>
+                <span className="font-medium">Welcome, {user.firstName}</span>
               </div>
+
+              {/* Dropdown content */}
               <ul
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
@@ -46,6 +56,6 @@ const NavBar = () => {
       </div>
     </>
   );
-}
+};
 
-export default NavBar
+export default NavBar;
